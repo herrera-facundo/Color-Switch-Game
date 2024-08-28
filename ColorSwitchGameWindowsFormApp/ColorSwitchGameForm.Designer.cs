736baef50a1575f85fc5ce33eaeff7d27c30fd56
Facundo Herrera Rivarola <facundotarg@hotmail.com>
2024-08-28 16:43:36 -0700

    Current version
    Previous versions were saved locally, therefore all files are pushed at once.

---
+++ ColorSwitchGameWindowsFormApp/ColorSwitchGameForm.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace ColorSwitchGameWindowsFormApp
+{
+    partial class ColorSwitchGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pcBxBackground = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTop = new System.Windows.Forms.Label();
+            this.txBxScores = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pcBxPlayer = new System.Windows.Forms.PictureBox();
+            this.pcBxBar1 = new System.Windows.Forms.PictureBox();
+            this.pcBxBar2 = new System.Windows.Forms.PictureBox();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCyan = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnPink = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnWhite = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBackground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcBxBackground
+            // 
+            this.pcBxBackground.BackColor = System.Drawing.Color.Black;
+            this.pcBxBackground.Location = new System.Drawing.Point(0, 0);
+            this.pcBxBackground.Name = "pcBxBackground";
+            this.pcBxBackground.Size = new System.Drawing.Size(230, 370);
+            this.pcBxBackground.TabIndex = 0;
+            this.pcBxBackground.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(235, 263);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 37);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start / Restart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(236, 13);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(72, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Your Score: 0";
+            // 
+            // lblTop
+            // 
+            this.lblTop.AutoSize = true;
+            this.lblTop.Location = new System.Drawing.Point(235, 49);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(80, 13);
+            this.lblTop.TabIndex = 3;
+            this.lblTop.Text = "Top 10 Scores:";
+            // 
+            // txBxScores
+            // 
+            this.txBxScores.Location = new System.Drawing.Point(236, 66);
+            this.txBxScores.Multiline = true;
+            this.txBxScores.Name = "txBxScores";
+            this.txBxScores.ReadOnly = true;
+            this.txBxScores.Size = new System.Drawing.Size(100, 191);
+            this.txBxScores.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(235, 306);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(101, 37);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pcBxPlayer
+            // 
+            this.pcBxPlayer.BackColor = System.Drawing.Color.White;
+            this.pcBxPlayer.Location = new System.Drawing.Point(50, 290);
+            this.pcBxPlayer.Name = "pcBxPlayer";
+            this.pcBxPlayer.Size = new System.Drawing.Size(130, 25);
+            this.pcBxPlayer.TabIndex = 6;
+            this.pcBxPlayer.TabStop = false;
+            // 
+            // pcBxBar1
+            // 
+            this.pcBxBar1.BackColor = System.Drawing.Color.Cyan;
+            this.pcBxBar1.Location = new System.Drawing.Point(0, 50);
+            this.pcBxBar1.Name = "pcBxBar1";
+            this.pcBxBar1.Size = new System.Drawing.Size(230, 25);
+            this.pcBxBar1.TabIndex = 7;
+            this.pcBxBar1.TabStop = false;
+            this.pcBxBar1.Tag = "Obstacle";
+            // 
+            // pcBxBar2
+            // 
+            this.pcBxBar2.BackColor = System.Drawing.Color.Yellow;
+            this.pcBxBar2.Location = new System.Drawing.Point(0, 150);
+            this.pcBxBar2.Name = "pcBxBar2";
+            this.pcBxBar2.Size = new System.Drawing.Size(230, 25);
+            this.pcBxBar2.TabIndex = 8;
+            this.pcBxBar2.TabStop = false;
+            this.pcBxBar2.Tag = "Obstacle";
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 10;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(30, 270);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 73);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            // 
+            // btnCyan
+            // 
+            this.btnCyan.BackColor = System.Drawing.Color.Aqua;
+            this.btnCyan.Location = new System.Drawing.Point(342, 153);
+            this.btnCyan.Name = "btnCyan";
+            this.btnCyan.Size = new System.Drawing.Size(85, 23);
+            this.btnCyan.TabIndex = 10;
+            this.btnCyan.UseVisualStyleBackColor = false;
+            this.btnCyan.Click += new System.EventHandler(this.btnCyan_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(342, 95);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(85, 23);
+            this.btnYellow.TabIndex = 11;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnPink
+            // 
+            this.btnPink.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnPink.Location = new System.Drawing.Point(342, 124);
+            this.btnPink.Name = "btnPink";
+            this.btnPink.Size = new System.Drawing.Size(85, 23);
+            this.btnPink.TabIndex = 12;
+            this.btnPink.UseVisualStyleBackColor = false;
+            this.btnPink.Click += new System.EventHandler(this.btnPink_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(343, 240);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(85, 23);
+            this.btnRed.TabIndex = 13;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(342, 211);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(85, 23);
+            this.btnBlue.TabIndex = 14;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Lime;
+            this.btnGreen.Location = new System.Drawing.Point(343, 182);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(85, 23);
+            this.btnGreen.TabIndex = 15;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnWhite
+            // 
+            this.btnWhite.BackColor = System.Drawing.Color.White;
+            this.btnWhite.Location = new System.Drawing.Point(342, 66);
+            this.btnWhite.Name = "btnWhite";
+            this.btnWhite.Size = new System.Drawing.Size(85, 23);
+            this.btnWhite.TabIndex = 16;
+            this.btnWhite.UseVisualStyleBackColor = false;
+            this.btnWhite.Click += new System.EventHandler(this.btnWhite_Click);
+            // 
+            // ColorSwitchGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 366);
+            this.Controls.Add(this.btnWhite);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnPink);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnCyan);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pcBxPlayer);
+            this.Controls.Add(this.pcBxBar2);
+            this.Controls.Add(this.pcBxBar1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txBxScores);
+            this.Controls.Add(this.lblTop);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pcBxBackground);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ColorSwitchGameForm";
+            this.Text = "Color Switch Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ColorSwitchGameForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBackground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBxBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcBxBackground;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.TextBox txBxScores;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pcBxPlayer;
+        private System.Windows.Forms.PictureBox pcBxBar1;
+        private System.Windows.Forms.PictureBox pcBxBar2;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCyan;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnPink;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnWhite;
+    }
+}
+
---
+++ ColorSwitchGameWindowsFormApp/ColorSwitchGameForm.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ColorSwitchGameWindowsFormApp
+{
+    public partial class ColorSwitchGameForm : Form
+    {
+        private const int OBSTACLE_SPEED = 2;
+        bool running = false;
+        int playerColorIndex = 0;
+        int currentScore = -1;
+        Random rng = new Random();
+        Color[] colors = { Color.White, Color.Red, Color.Blue, Color.Green, Color.Cyan, Color.Magenta, Color.Yellow };
+        string filePath;
+
+        public ColorSwitchGameForm()
+        {
+            InitializeComponent();
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDataPath = System.IO.Path.Combine(appDataPath, "ColorSwitchGame");
+            System.IO.Directory.CreateDirectory(appDataPath);
+            filePath = System.IO.Path.Combine(appDataPath, "HighScores.txt");
+            if (!System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Create(filePath).Dispose();
+            }
+            MessageBox.Show("Instructions: use the arrow keys to match the color of the incoming bars");
+            updateScores();
+        }
+
+        private void updateScores()
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                txBxScores.Text = "";
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string[] parts = line.Split(',');
+                    txBxScores.AppendText($"{parts[0]} {parts[1],6}\r\n");
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public static string GetInitials(string title, string text)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 140,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = title,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 10, Top = 10, Text = text, Width = 250 };
+            TextBox textBox = new TextBox() { Left = 10, Top = 30, Width = 250 };
+            Button okButton = new Button()
+            {
+                Text = "OK",
+                Left = 10,
+                Width = 50,
+                Top = 60,
+                DialogResult = DialogResult.OK
+            };
+            okButton.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(okButton);
+            prompt.Controls.Add(textLabel);
+            prompt.MaximizeBox = false;
+            prompt.MinimizeBox = false;
+            prompt.AcceptButton = okButton;
+            prompt.ShowDialog();
+
+            return textBox.Text.Trim();
+        }
+
+        private void resetGame()
+        {
+            currentScore = 0;
+            lblScore.Text = "Your score: " + currentScore.ToString();
+            playerColorIndex = 0;
+            pcBxPlayer.BackColor = colors[0];
+            pcBxBar1.Top = -10;
+            pcBxBar1.BackColor = colors[6];
+            pcBxBar2.Top = -110;
+            pcBxBar2.BackColor = colors[4];
+            pcBxPlayer.Focus();
+            tmr.Start();
+        }
+
+        private void ColorSwitchGameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Right) && (running))
+            {
+                if (playerColorIndex < colors.Length - 1)
+                {
+                    playerColorIndex++;
+                }
+                else
+                {
+                    playerColorIndex = 0;
+                }
+                pcBxPlayer.BackColor = colors[playerColorIndex];
+            }
+            if ((e.KeyCode == Keys.Left) && (running))
+            {
+                if (playerColorIndex > 0)
+                {
+                    playerColorIndex--;
+                }
+                else
+                {
+                    playerColorIndex = colors.Length - 1;
+                }
+                pcBxPlayer.BackColor = colors[playerColorIndex];
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            resetGame();
+            running = true;
+            SendKeys.SendWait("{Left}");
+            SendKeys.SendWait("{Left}");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c.Tag != null)
+                    {
+                        c.Top += OBSTACLE_SPEED;
+                        // check if obstacles are off screen
+                        if (c.Top > ActiveForm.Size.Height)
+                        {
+                            c.Top = rng.Next(-110, -9);
+                            c.BackColor = colors[rng.Next(0, colors.Length)];
+                            currentScore++;
+                            lblScore.Text = "Your score: " + currentScore.ToString();
+                        }
+                        if (((c.Top + c.Height) > pcBxPlayer.Top) && (c.Top + c.Height < (pcBxPlayer.Top + pcBxPlayer.Height)) && (pcBxPlayer.BackColor != c.BackColor))
+                        {
+                            tmr.Stop();
+                            running = false;
+                            checkScore();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkScore()
+        {
+            try
+            {
+                string[] scores = new string[10];
+                int lineNum = 0;
+                bool scoreUpdated = false;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        string currentInitials = GetInitials("New High Score!", "Enter your initials:");
+                        scores[0] = currentInitials + "," + currentScore;
+                    }
+                    else
+                    {
+                        while ((lineNum < 10) && (line != null))
+                        {
+                            string[] parts = line.Split(',');
+                            int fileScore = int.Parse(parts[1]);
+
+                            if ((currentScore > fileScore) && (!scoreUpdated))
+                            {
+                                string currentInitials = GetInitials("New High Score!", "Enter your initials:");
+                                scores[lineNum] = currentInitials + "," + currentScore;
+                                lineNum++;
+                                scoreUpdated = true;
+                            }
+                            if (lineNum == 10)
+                            {
+                                break;
+                            }
+                            scores[lineNum] = line;
+                            line = reader.ReadLine();
+                            lineNum++;
+                        }
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (string score in scores)
+                    {
+                        if (score != null)
+                        {
+                            writer.WriteLine(score);
+                        }
+                    }
+                }
+                updateScores();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found, here's a bunch of stuff: " + e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.Out.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        private void btnWhite_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[0];
+            }
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[6];
+            }
+        }
+
+        private void btnPink_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[5];
+            }
+        }
+
+        private void btnCyan_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[4];
+            }
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[3];
+            }
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[2];
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                pcBxPlayer.BackColor = colors[1];
+            }
+        }
+    }
+}
